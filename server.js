a716790f7bd79757fd0cdb8086575e99b534356e
Kalgarn <kalgarn@tuta.io>
2016-01-29 16:40:38 +0100

    node server setup and hello world

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var port = process.env.port || 8080;
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname,'client','views'));
+
+app.get('/', function(req,res){
+	res.render('index.ejs');
+});
+
+app.listen(port, function(){
+	console.log('SERVER IS RUNNING... PORT:' + port);
+});
