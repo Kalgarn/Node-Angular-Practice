2f7c535436e8fc50128e3d0cf06594cce34ac7ab
Kalgarn <kalgarn@tuta.io>
2016-01-31 12:03:08 +0100

    angular-ui bootstrap carrousel

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,6 @@
 var myApp = angular.module('myApp', [
-	'ngRoute']).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+	'ngRoute',
+	'ui.bootstrap']).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
 	$routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeController'});
 	$routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'aboutController'});
 	$routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'contactController'});
--- client/js/controllers/homeController.js
+++ client/js/controllers/homeController.js
@@ -1,3 +1,17 @@
 myApp.controller('homeController', ['$scope', function($scope){
-
+	$scope.slides = [{
+		image: "imgs/angular-logo.jpeg"
+	},
+	{
+		image: "imgs/nodejs_logo.png"
+	},
+	{
+		image: "imgs/AngularJS-large-resized.png"
+	},
+	{
+		image: "imgs/nodejs-mongodb.png"
+	},
+	{
+		image: "imgs/Angular-2-beta.jpg"
+	}]
 }]);
