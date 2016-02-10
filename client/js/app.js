ce557f7ee744e4d617c6bd6d31fd649523f5879b
Kalgarn <kalgarn@tuta.io>
2016-02-10 13:46:29 +0100

    ngAnimate lib and projectsController

--- client/js/app.js
+++ client/js/app.js
@@ -2,7 +2,7 @@ var myApp = angular.module('myApp', [
 	'ngRoute',
 	'ui.bootstrap',
     'ngResource',
-    'ngAnimmate']).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    'ngAnimate']).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 	$routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeController'});
 	$routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'aboutController'});
 	$routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'contactController'});
--- server/models/customer.js
+++ server/models/customer.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var customerSchema = mongoose.Schema({
-   firstname: String,
-    lastname: String,
-    phone: String,
-    address: {
-        street: String,
-        city: String
-    }
+        firstname: String,
+        lastname: String,
+        phone: String,
+        address: {
+            street: String,
+            city: String
+        }
     
-});
+    });
 
 module.exports = mongoose.model('customer', customerSchema);
